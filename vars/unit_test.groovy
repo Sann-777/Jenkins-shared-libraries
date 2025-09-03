65108c543cd4b78e44fa6eb5384f6115c506b745
Sandeep Singh <contactdevsandeep@gmail.com>
2025-09-03 17:36:39 +0530

    Update unit_test.groovy

--- vars/unit_test.groovy
+++ vars/unit_test.groovy
@@ -1,6 +1,14 @@
 def call(){
+  echo "insatlling required dependencies"
+  sh 'npm run ci:setup'
+  echo "dependencies installed."
+  
   echo "Testing each services."
   sh 'npm run install:all'
   sh 'npm run test:services:ready'
   echo "Testing completed."
+
+  echo "Testing lint and style"
+  sh 'npm run lint:services:ready'
+  echo "Testing complete"
 }
